8daf7f42ac4751096b80cce263a5c9ae795b48d0
Thaigo_Freitas <tmfreitas@outlook.com>
2018-02-28 00:37:53 -0300

    create factories

--- spec/factories/questions_answers.rb
+++ spec/factories/questions_answers.rb
@@ -1,7 +1,20 @@
+
 FactoryGirl.define do
-  factory :questions_answer do
-    value "MyText"
-    answer nil
-    question nil
+  timestamp = loop do
+     date = FFaker::Time.date.to_time
+     break date.strftime("%F %T") if date &lt;= Date.today
+  end
+
+
+factory :user do
+    uid          { FFaker::Lorem.word }
+    email        { FFaker::Internet.email }
+    name         { FFaker::Name.name }
+    nickname     { FFaker::Internet.user_name }
+    password     { FFaker::Lorem.word }
+    provider     'email'
+    confirmed_at timestamp
+    created_at   timestamp
+    updated_at   timestamp
   end
 end
