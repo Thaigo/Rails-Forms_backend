9db6f203a66d8cb8ff387a04ca0e4b2cac4dd89f
Thaigo_Freitas <tmfreitas@outlook.com>
2018-03-03 00:04:03 -0300

    setando os models

---
+++ db/migrate/20180303025331_add_slug_toforms.rb
@@ -0,0 +1,6 @@
+class AddSlugToforms < ActiveRecord::Migration[5.0]
+  def change
+    add_column :forms, :slug, :string
+    add_index :forms, :slug, unique: true
+  end
+end
--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,8 +1,5 @@
 FactoryGirl.define do
   factory :answer do
-    user nil
-    form nil
-    user
     form
   end
 end
