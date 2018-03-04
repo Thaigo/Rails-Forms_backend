4469de63dc94b22f01201b7b1f3dc084d9f41bc9
Thaigo_Freitas <tmfreitas@outlook.com>
2018-03-04 00:22:31 -0300

    create actions to controllers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -28,12 +28,15 @@ require 'rspec/rails'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  # Custom json helpers
+  config.include Requests::JsonHelpers, type: :request
+  # Custom Header helpers
+  config.include Requests::HeaderHelpers, type: :request
+ 
+  config.include Devise::Test::ControllerHelpers, type: :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.include Requests::JsonHelpers, type: :request
-# Custom Header helpers
-  config.include Requests::HeaderHelpers, type: :request
-
+ 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
